bf0713fa1c960d12b9d025f011397e8b0b511cd6
Argha <arghacious@gmail.com>
2024-03-27 13:21:27 +0530

    PWA Changes

--- script.js
+++ script.js
@@ -153,4 +153,16 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+              console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+              console.error('Service Worker registration failed:', error);
+            });
+        });
+      }
 });
