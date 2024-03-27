b3bc6ae8cb4ce907ba0ac92772583d3da9bde96c
Argha <arghacious@gmail.com>
2024-03-27 13:42:04 +0530

    service-worker.js Added

---
+++ service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'cryptopaths-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/APP_LOGO/app_192.png',
+  '/APP_LOGO/app_180.png',
+  '/APP_LOGO/app_512.png',
+  '/APP_LOGO/app_1024.png',
+  '/FRONT_NEW.jpg',
+  '/SKY_TOP.jpg',
+  // Add more URLs to cache as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to ensure it can be used multiple times
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          response => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to ensure it can be used multiple times
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
